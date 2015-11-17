04cc86d40403980e4e5738af30284e10f17dea7c
kpalkowska <kpalkowska@sigma.ug.edu.pl>
2015-11-17 16:53:20 +0100

    Update euklides.rb

--- zad_2/lib/euklides.rb
+++ zad_2/lib/euklides.rb
@@ -1,11 +1,11 @@
-def oblicz_nwd(a,b,p=0)
+def euklides(a , b, p=0)
   if a == b
   end
   count = 0
   while (a != b)
-    count += 1
+    count = count + 1
     if a < b
-      b = b-a
+      b = b - a
     else
       a = a - b
     end
