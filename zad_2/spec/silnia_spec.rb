ad6868983c8c66b158b3d288da2670156247ae26
Klaudia <kpalkowska@sigma.inf.ug.edu.pl>
2015-11-17 16:47:23 +0100

    update silnia_spec.rb

--- zad_2/spec/silnia_spec.rb
+++ zad_2/spec/silnia_spec.rb
@@ -6,19 +6,23 @@ require_relative '../lib/silnia'
 
 RSpec.describe 'silnia.rb' do
 describe '#silnia' do
-  it 'Should be defined' do
-    expect { silnia(122) }.not_to raise_error
+
+  it 'should be defined' do
+    expect { silnia(369) }.not_to raise_error
   end
-    it 'should never be equal to factorial.' do
-       expect(silnia(1)).to eq(1)
-       expect(silnia(5)).to eq(120)
-    end
-    it 'should always be equal to factorial.' do
-      expect(silnia(4)).not_to eq(8)
-      expect(silnia(1)).not_to eq(0)
-    end
-    it 'should be defined' do
-      expect(silnia(0)).to eq(1)
-    end
+
+  it 'should never be equal to factorial.' do
+    expect(silnia(1)).to eq(1)
+    expect(silnia(5)).to eq(120)
+  end
+
+  it 'should always be equal to factorial.' do
+    expect(silnia(4)).not_to eq(8)
+    expect(silnia(1)).not_to eq(0)
+  end
+
+  it 'should be defined' do
+    expect(silnia(0)).to eq(1)
+  end
 end
 end
